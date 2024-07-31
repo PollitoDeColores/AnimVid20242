40e99fe37c597c0f185458d289520805053359a5
B09S304est <>
2024-07-30 19:26:21 -0500

    Changed Things

--- Assets/Scritpts/CharacterMovement.cs
+++ Assets/Scritpts/CharacterMovement.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
 
 public class CharacterMovement : MonoBehaviour
 {
+    [SerializeField] private MovementDamp motion;
     Animator animator;
     int animatorXID;
     int animatorYID;
 
+    public void Movement(CallbackContext context)
+    {
+        Vector2 motionDirection = context.ReadValue<Vector2>();
+        motion.TargetValue = motionDirection;
+    }
+    private void Update()
+    {
+        motion.DoSomething();
+        animator.SetFloat(animatorYID, motion.CurrentValue.y);
+        animator.SetFloat(animatorXID, motion.CurrentValue.x);
+    }
     private void Awake()
     {
         animatorXID = Animator.StringToHash("X");
@@ -17,11 +31,7 @@ public class CharacterMovement : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+
     }
 
-    public void Move(Vector3 motionDirection)
-    {
-        animator.SetFloat(animatorYID, motionDirection.y);
-        animator.SetFloat(animatorXID, motionDirection.x);
-    }
 }
