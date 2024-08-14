3b1b89c7fda37c0a844aa9cfd84e6829518aa777
B09S304est <>
2024-08-13 19:39:27 -0500

    Clase 13/08 final

--- Assets/Scripts/AimController.cs
+++ Assets/Scripts/AimController.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
 
 public class AimController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform _aimRig;
+    [SerializeField] private MovementDamp LookVector;
+    private bool val = false;
+    [SerializeField] private AimConstraint _chestAim;
+
+    void Awake()
     {
-        
+        _aimRig.gameObject.SetActive(val);     
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Aim(InputAction.CallbackContext context){
+        val = context.ReadValueAsButton();
+        _chestAim.enabled = val; 
+    }
+
+    public void Look(InputAction.CallbackContext context){
+        LookVector.TargetValue = context.ReadValue<Vector2>();
+    }
+
+    private void Update() {
+        LookVector.DoSomething();
+        _aimRig.RotateAround(_aimRig.position, transform.up, LookVector.CurrentValue.x);
     }
 }
