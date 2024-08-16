97aee0015624a3330f39c6832938ba46fbf4bb01
B09S304est <>
2024-08-15 19:31:46 -0500

    yes

--- Assets/Script/AimController.cs
+++ Assets/Script/AimController.cs
@@ -8,21 +8,27 @@ public class AimController : MonoBehaviour
     [SerializeField] private AimConstraint chestAim;
     [SerializeField] private Transform aimRig;
     [SerializeField] private Transform _camera;
+    Animator _animator;
+    bool aiming;
 
     public void Aim(InputAction.CallbackContext ctx)
     {
         bool val = ctx.ReadValueAsButton();
+        aiming = val;
         chestAim.enabled = val;
         aimRig.gameObject.SetActive(val); 
+        _animator.SetBool("Aiming", val);
     }
 
     private void Awake()
     {
         aimRig.gameObject.SetActive(false); 
+        _animator = GetComponent<Animator>();	
     }
 
     void Update()
     {
-        transform.rotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(_camera.forward, transform.up).normalized);
+        if(aiming)
+            transform.rotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(_camera.forward, transform.up).normalized);
     }
 }
