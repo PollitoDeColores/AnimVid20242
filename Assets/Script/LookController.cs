350fb1be62c5d282c6441d4d5b1115793e58e1a0
iSekkachi <contact.coloredchick@gmail.com>
2024-08-20 18:28:44 -0500

    part 1 class 20/08

--- Assets/Script/LookController.cs
+++ Assets/Script/LookController.cs
@@ -7,7 +7,8 @@ public class LookController : MonoBehaviour
     [SerializeField] private VectorDampener lookVector;
     [SerializeField] private Transform lookRig;
     [SerializeField] private float sensitivity;
-
+    [SerializeField] private Vector2 _clamp;
+    private float vertRotation;
 
     public void Look(InputAction.CallbackContext ctx)
     {
@@ -15,12 +16,15 @@ public class LookController : MonoBehaviour
 
     }
 
-
-
     private void Update()
     {
         lookVector.Update();
         lookRig.RotateAround(lookRig.position, transform.up, lookVector.CurrentValue.x * sensitivity * 360f);
+        vertRotation -= lookVector.CurrentValue.y*sensitivity*360;
+        vertRotation = Mathf.Clamp(vertRotation, _clamp.x, _clamp.y);
+        Vector3 _euler = lookRig.localEulerAngles;
+        lookRig.localEulerAngles = new Vector3(vertRotation, _euler.y, _euler.z);
+        //lookRig.RotateAround(lookRig.position, lookRig.right, lookVector.CurrentValue.x * sensitivity * 360f);
     }
 
 }
